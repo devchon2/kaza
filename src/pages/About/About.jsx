f5d19163609d24fa719fbedd74a7083cab14493e
Rachid Chon <rachidchon.adm@hotmail.fr>
2023-09-02 13:58:37 +0200

    Mise en place Routage pour les pages + ajout Header et Footer

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,13 @@
+import Footer from '../../components/Footer/Footer.jsx';
+import Header from '../../components/Header/Header'
+
+
+export default function About(){
+  return (
+    <div className="About">
+      <Header />
+      <div>Page About</div>
+      <Footer />
+    </div>
+  );
+}
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+
+export default function HomePage(){
+  return (
+    <div className="HomePage">
+      <Header />
+      <div>Page HomePage</div>
+      <Footer />
+    </div>
+  );
+}
