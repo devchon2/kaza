65436731184fd70418359621ef58b68220ec2cd8
Rachid Chon <rachidchon.adm@hotmail.fr>
2023-09-13 12:11:29 +0200

    fin validation affinage css et erreur

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage'
 import AboutPage from './pages/AboutPage/AboutPage'
