1cf24222f1611b3322a21a17be9a203e7253d449
Rachid Chon <rachidchon.adm@hotmail.fr>
2023-09-13 15:26:47 +0200

    finalisation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -7,11 +7,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(    
-<StrictMode>
+
     <BrowserRouter>
       <App />
-    </BrowserRouter>    
-    </StrictMode>
-
+    </BrowserRouter>
 )      
 
