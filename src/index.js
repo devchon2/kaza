6a452ba1cc8863a1f2aa2a5d4342d23af20e7e37
Rachid Chon <rachidchon.adm@hotmail.fr>
2023-09-13 12:48:18 +0200

    restore strictMode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -7,9 +7,11 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(    
-
+<StrictMode>
     <BrowserRouter>
       <App />
-    </BrowserRouter>
+    </BrowserRouter>    
+    </StrictMode>
+
 )      
 
